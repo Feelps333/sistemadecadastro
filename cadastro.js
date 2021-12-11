e522e243756549c74427912c43fef59670e06a1e
Feelps333 <antoniofelipesilva@live.com>
2021-12-11 09:34:15 -0300

    SistemaCadastro

---
+++ cadastro.js
@@ -0,0 +1,17 @@
+let dataEvento = [11 / 12 / 2021]
+
+if (dataEvento.length >= 11 / 12 / 2021) {
+  console.log('Data Valida')
+}
+
+let idade = [18]
+
+if (idade >= 18) {
+  console.log('Idade Valida')
+}
+
+let vagas = [100]
+
+if (vagas.length < 100) {
+  console.log('Vagas Disponivel')
+}
